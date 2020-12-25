1abf864a245c159fd20139fd1df7f05e93909994
jahedev <jahedev@gmail.com>
2020-12-25 15:32:35 -0500

    tic-tac-toe game is now completely working

--- 1_tic-tac-toe/script.js
+++ 1_tic-tac-toe/script.js
@@ -2,9 +2,11 @@
 const container = document.querySelector('.container');
 const boxes = document.querySelectorAll('.tile');
 const playAgain = document.querySelector('.btn');
+const winner = document.querySelector('#winner');
 /* #endregion */
 
-/* #region Declare Elements */
+/* #region Game Functions */
+
 var isX = true;
 function XO() {
   let XO = isX ? 'X' : 'O';
@@ -12,13 +14,11 @@ function XO() {
   return XO;
 }
 
-/* #endregion */
-
-/* #region Game Functions */
-
 function resetGame() {
   boxes.forEach((box) => {
     box.innerHTML = '';
+    winner.innerHTML = '';
+    isX = true;
   });
 }
 function showPlayAgain(shouldShow) {
@@ -42,9 +42,11 @@ function gameFinisher() {
     return;
   }
 
-  let gameWonBy = gameWonBy();
-  if (gameWonBy() === 'X') {
-  } else if (gameWonBy() === 'O') {
+  let gameWonBy = whoWon();
+
+  if (gameWonBy === 'X' || gameWonBy === 'O') {
+    winner.innerText = gameWonBy + ' won the game';
+    showPlayAgain(true);
   }
 }
 
@@ -52,15 +54,26 @@ function allEqual(arr) {
   return arr.every((val, i, arr) => val === arr[0]);
 }
 
-function gameWonBy() {
-  const B = [];
+function whoWon() {
+  let B = [];
   boxes.forEach((box) => {
     B.push(String(box.innerText));
   });
   // row 1, 2, and 3
   if (B[0] !== '' && allEqual(B.slice(0, 3))) return B[0];
-  if (B[3] !== '' && allEqual(B.slice(3, 6))) return B[0];
-  if (B[6] !== '' && allEqual(B.slice(6))) return B[0];
+  if (B[3] !== '' && allEqual(B.slice(3, 6))) return B[3];
+  if (B[6] !== '' && allEqual(B.slice(6))) return B[6];
+
+  // column 1, 2, and 3
+  if (B[0] !== '' && allEqual([B[0], B[3], B[6]])) return B[0];
+  if (B[1] !== '' && allEqual([B[1], B[4], B[7]])) return B[1];
+  if (B[2] !== '' && allEqual([B[2], B[5], B[8]])) return B[2];
+
+  // diagnols
+  if (B[0] !== '' && allEqual([B[0], B[4], B[8]])) return B[0];
+  if (B[2] !== '' && allEqual([B[2], B[4], B[6]])) return B[2];
+
+  return '';
 }
 
 /* #endregion */
@@ -69,7 +82,7 @@ function gameWonBy() {
 container.addEventListener('click', (e) => {
   if (e.target.classList == 'tile') {
     if (e.target.innerText == '') {
-      e.target.innerText = XO();
+      if (winner.innerText == '') e.target.innerText = XO();
     }
   }
   gameFinisher();
