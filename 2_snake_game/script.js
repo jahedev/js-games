edad7b0734f61056bf3dddf15206ca3d239880ea
jahedev <jahedev@gmail.com>
2021-03-26 12:03:31 -0400

    snake game loop

--- 2_snake_game/script.js
+++ 2_snake_game/script.js
@@ -4,7 +4,13 @@ let grid = document.getElementById('grid')
 // Game Variables
 let gridSize = 16
 let gameEnded = false
+const snakeBody = [{ x: 11, y: 11 }]
 
+// Game Loop Variables
+let lastRenderTime = 0
+const SNAKE_SPEED = 2
+
+// Generates div grid for game
 function generateGrid(size) {
   grid.innerHTML = ''
   for (let row = 1; row <= size; row++) {
@@ -20,7 +26,29 @@ function restartGame() {
   generateGrid(gridSize)
 }
 
+function game(currentTime) {
+  window.requestAnimationFrame(game)
+
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+  console.log(secondsSinceLastRender)
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+function update() {
+  console.log('update')
+}
+
+function draw() {
+  console.log('draw')
+}
+
 ;(function main() {
   // Initialize Game
   generateGrid(gridSize)
+  // window.requestAnimationFrame(game)
 })()
