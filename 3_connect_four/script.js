6938e4e30f2c43c26605dcd7dfadeb4c45ceb6dc
Mohammed J. Hossain <jahedev@gmail.com>
2021-04-01 15:41:50 -0400

    fixed cross diagnols 4 in a row bug

--- 3_connect_four/script.js
+++ 3_connect_four/script.js
@@ -383,6 +383,56 @@ function checkWinningDiagnols() {
 
     winner = fourInRow(grid[5][3], grid[4][4], grid[3][5], grid[2][6])
 
+    if (!!winner) break
+
+    // Cross Diagnols
+
+    /*
+     * grid[2][0], grid[3][1], grid[4][2], grid[5][3]
+     * grid[1][0], grid[2][1], grid[3][2], grid[4][3], grid[5][4]
+     * grid[0][0], grid[1][1], grid[2][2], grid[3][3], grid[4][4], grid[5][5]
+     * grid[0][1], grid[1][2], grid[2][3], grid[3][4], grid[4][5], grid[5][6]
+     * grid[0][2], grid[1][3], grid[2][4], grid[3][5], grid[4][6]
+     * grid[0][3], grid[1][4], grid[2][5], grid[3][6]
+     */
+
+    winner = fourInRow(grid[2][0], grid[3][1], grid[4][2], grid[5][3])
+    if (!!winner) break
+    winner = fourInRow(
+      grid[1][0],
+      grid[2][1],
+      grid[3][2],
+      grid[4][3],
+      grid[5][4]
+    )
+    if (!!winner) break
+    winner = fourInRow(
+      grid[0][0],
+      grid[1][1],
+      grid[2][2],
+      grid[3][3],
+      grid[4][4],
+      grid[5][5]
+    )
+    if (!!winner) break
+    winner = fourInRow(
+      grid[0][1],
+      grid[1][2],
+      grid[2][3],
+      grid[3][4],
+      grid[4][5],
+      grid[5][6]
+    )
+    if (!!winner) break
+    winner = fourInRow(
+      grid[0][2],
+      grid[1][3],
+      grid[2][4],
+      grid[3][5],
+      grid[4][6]
+    )
+    if (!!winner) break
+    winner = fourInRow(grid[0][3], grid[1][4], grid[2][5], grid[3][6])
     break
   }
 
