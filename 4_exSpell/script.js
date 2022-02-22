d220da146da9d2c457d22210a1134c2da1585ab7
jahedev <jahedev@gmail.com>
2022-02-22 11:15:53 -0500

    kanva

--- 4_exSpell/script.js
+++ 4_exSpell/script.js
@@ -1,1 +1,28 @@
-// a
+// first we need to create a stage
+var stage = new Konva.Stage({
+  container: 'game-canvas',
+  width: 800,
+  height: 800,
+})
+
+// then create layer
+var layer = new Konva.Layer()
+
+// create our shape
+var circle = new Konva.Circle({
+  x: stage.width() / 2,
+  y: stage.height() / 2,
+  radius: 70,
+  fill: 'red',
+  stroke: 'black',
+  strokeWidth: 4,
+})
+
+// add the shape to the layer
+layer.add(circle)
+
+// add the layer to the stage
+stage.add(layer)
+
+// draw the image
+layer.draw()
